e9edf9781326c20d54cdc164edf63016a0def903
hustler hustler <hustlerh3848@gmail.com>
2025-07-11 03:26:02 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <LinkComponent hre

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -17,9 +17,9 @@ export function Header() {
     <header className="sticky top-0 z-50 w-full glassmorphic">
       <div className="container flex h-16 max-w-screen-2xl items-center">
         <div className="mr-4 flex">
-          <Link href="/" className="mr-6 flex items-center space-x-2 p-2">
+          <Link href="/" className="mr-6 flex items-center space-x-2">
             <CodeXml className="h-6 w-6 text-accent" />
-            <span className="font-bold font-headline sm:inline-block">
+            <span className="font-bold font-headline sm:inline-block text-foreground">
               CodeSnippr
             </span>
           </Link>
